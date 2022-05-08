bd4efdf3d96cc72f0dca620261c42a061099e65e
josefl <jose.fl_@hotmail.com>
2022-05-08 11:40:21 -0500

    profileComponente con renovacion de suscripcion (controlador de Account)

---
+++ force-app/main/default/lwc/profileComponenteWOU/profileComponenteWOU.js
@@ -0,0 +1,5 @@
+import { LightningElement, api } from 'lwc';
+
+export default class ProfileComponenteWOU extends LightningElement {
+    @api recordId;
+}
---
+++ force-app/main/default/lwc/renovateSuscriptionComponentWOU/renovateSuscriptionComponentWOU.js
@@ -0,0 +1,9 @@
+import { LightningElement, api } from 'lwc';
+
+export default class RenovateSuscriptionComponentWOU extends LightningElement {
+    @api recordId;
+
+    handleClick(event) {
+        
+    }
+}
