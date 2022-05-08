f0aee9c9996b84f590ebefd35908fdf183d6bcb9
josefl <jose.fl_@hotmail.com>
2022-05-08 14:13:49 -0500

    Metodo para determinar tipo de usuario y cargar el componente necesario

--- force-app/main/default/lwc/profileComponenteWOU/profileComponenteWOU.js
+++ force-app/main/default/lwc/profileComponenteWOU/profileComponenteWOU.js
@@ -1,5 +1,14 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+
+import isUser from '@salesforce/apex/AccountControllerWOU.isUser';
 
 export default class ProfileComponenteWOU extends LightningElement {
     @api recordId;
+
+    @wire(isUser, {id: '$recordId'})
+    userComp;
+
+    get userComp() {
+        return this.userComp.data.RecordType.Name.value;
+    }
 }
