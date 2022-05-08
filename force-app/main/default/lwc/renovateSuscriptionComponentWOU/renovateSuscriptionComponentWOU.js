2e9cd3c32ff3cf6dcbb58831b5907be6ffc15b22
josefl <jose.fl_@hotmail.com>
2022-05-08 13:06:53 -0500

    update source de componente renovateSuscription

--- force-app/main/default/lwc/renovateSuscriptionComponentWOU/renovateSuscriptionComponentWOU.js
+++ force-app/main/default/lwc/renovateSuscriptionComponentWOU/renovateSuscriptionComponentWOU.js
@@ -1,9 +1,36 @@
-import { LightningElement, api } from 'lwc';
+import { LightningElement, api, wire } from 'lwc';
+import updateSuscription from '@salesforce/apex/AccountControllerWOU.updateSuscription';
+import renewAvailable from '@salesforce/apex/AccountControllerWOU.renewAvailable';
 
 export default class RenovateSuscriptionComponentWOU extends LightningElement {
     @api recordId;
 
+    @wire(renewAvailable, {id: '$recordId'})
+    available;
+
+    get available() {
+        return this.available.data.Suscription_status__c.value;
+    }
+
     handleClick(event) {
-        
+        updateSuscription({id: this.recordId})
+        .then((result) => {
+            if(result){                         
+                console.log(result);
+                window.location.href = result;
+            }
+        }).catch((error) => {
+            console.log('Error en then')
+            console.log('error-', error.body);
+
+            if(error && error.body && error.body.message){
+                this.dispatchEvent(new ShowToastEvent({
+                        title:'Error',
+                        message: error.body.message,
+                        variant:'error'
+                    }),
+                );
+            }
+        });
     }
 }
