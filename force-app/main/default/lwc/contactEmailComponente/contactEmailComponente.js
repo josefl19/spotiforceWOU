44688ac132d18eadf08d1e36ab43da653e6270fe
josefl <jose.fl_@hotmail.com>
2022-05-11 01:27:13 -0500

    diseño para componente de envio de correo email-to-case terminado

--- force-app/main/default/lwc/contactEmailComponente/contactEmailComponente.js
+++ force-app/main/default/lwc/contactEmailComponente/contactEmailComponente.js
@@ -1,3 +1,5 @@
 import { LightningElement } from 'lwc';
 
-export default class ContactEmailComponente extends LightningElement {}
+export default class ContactEmailComponente extends LightningElement {
+    email = 'jose.devtesting@x90jy6ad7mcb1h6i0j774i8fgb6zxhy017wqcydkuzqiy9tll.iy-b9852ac.fra19.case.salesforce.com';
+}
