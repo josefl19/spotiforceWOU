095efc0393f6c1b7e77f909cbb6583289f559d5f
josefl <jose.fl_@hotmail.com>
2022-05-11 13:42:06 -0500

    carga del template de correo de bienvenida

--- force-app/main/default/lwc/webToCaseComponentWOU/webToCaseComponentWOU.js
+++ force-app/main/default/lwc/webToCaseComponentWOU/webToCaseComponentWOU.js
@@ -1,3 +1,14 @@
 import { LightningElement } from 'lwc';
+import {ShowToastEvent} from 'lightning/platformShowToastEvent';
 
-export default class WebToCaseComponentWOU extends LightningElement {}
+export default class WebToCaseComponentWOU extends LightningElement {
+    handleClick(event) {
+        this.dispatchEvent(new ShowToastEvent({
+                title: 'Form sent successfully',
+                message: 'We will contact you shortly',
+                variant: 'success'
+            })
+        );
+        window.location.href = 'https://wise-raccoon-kefkh5-dev-ed.preview.salesforce-communities.com/spotiforceWU/s/account/001IY000002OH1kYAG/edge-communications?app=commeditor';
+    }
+}
